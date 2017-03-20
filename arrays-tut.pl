94df458c15313c5fa910ea01ff8d0205425a7d7a
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2017-03-20 11:16:08 -0500

    array lesson

--- arrays-tut.pl
+++ arrays-tut.pl
@@ -1,17 +1,18 @@
 use strict;
 use warnings;
-
 use LWP::Simple;
 
+$| = 1;
+
 main {
-  my $url = 'http://www.theresa.codes'
-  my $file = 'index.html'
-  # if index.html has already been downloaded
-  if (-f 'index.html') {
-    print ('File found!\n');
-  } else {
-    getstore($url, $file);
-    print ('New copy of %s from %s has been downloaded.', $file $url);
+  my @files = ("index.html", "test.html");
+
+  foreach my $file(@files){
+    if (-f $file) {
+      print ("File found! $file\n");
+    } else {
+      print("File not found: $file \n");
+    }
   }
 }
 
